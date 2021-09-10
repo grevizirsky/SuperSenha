a4c894d40b5df28930e14e7d46f90d393cdef3e0
vpn <vpn@NBBV030975.local>
2021-09-10 00:15:00 -0300

    primeiro commit

---
+++ SuperSenha/SecurityTipsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecurityTipsViewController.swift
+//  SuperSenha
+//
+//  Created by vpn on 08/09/21.
+//
+
+import UIKit
+
+class SecurityTipsViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+   
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+     
+
+}
